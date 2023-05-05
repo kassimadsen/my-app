e1a9e3cca0584630da77bdb6a1c845bec861894a
Kassandra Madsen <madsenkassi@gmail.com>
2023-05-04 20:18:20 -0600

    First custom component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import ExpenseItem from "./components/ExpenseItem";
+// root component
 function App() {
+  // How you would do it in javascript: (imperative way)
+  // const para = document.createElement('p');
+  // para.textContent = 'This is visible!';
+  // document.getElementById('root').append(para);
+  // How you would convert that to react:
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <p>This is also visible!</p>
+  //   </div>
+  // );
   return (
+    // jsx code
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
