10888fec17f3539cba4d84391fe7ae2a837b94c5
Kassandra Madsen <madsenkassi@gmail.com>
2023-05-07 21:26:01 -0600

    event handlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 function ExpenseItem(props) {
   // must have only one root element per return statement/jsx code snippet
+  const clickHandler = () => {
+    console.log("Clicked!!!");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -11,6 +14,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
